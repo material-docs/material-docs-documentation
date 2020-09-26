e659666ae5dd3891a7e0ad0bb1b2472317e4b195
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-09-27 01:50:21 +0300

    [Tutorials] Added Hosting on GitHub Pages tutorial.

--- src/pages/Tutorials/HostingMaterialDocsOnGHPages/index.js
+++ src/pages/Tutorials/HostingMaterialDocsOnGHPages/index.js
@@ -10,11 +10,13 @@ import Markdown from "@material-docs/core/components/Markdown/Markdown";
 import content from "./HostingMaterialDocsOnGHPages.md";
 import GitHubPagesInitialImage from "./images/GitHubPagesInitialImage.png";
 import GitHubPagesSetupImage from "./images/GitHubPagesSetupImage.png";
+import DNSSetup from "./images/DNSSetup.png";
+import GitHubPagesDNSSetup from "./images/GitHubPagesDNSSetup.png";
 
 export default function HostingMaterialDocsOnGHPages() {
     return (
         <DocsPage
-            name={"Hosting on gh-pages"}
+            name={"Hosting on GitHub Pages"}
             searchTags={["create", "material", "docs", "tutorial", "faq", "manual"]}
             searchLabel={"Hosting you Material Docs based documentation on GitHub pages"}
         >
@@ -23,6 +25,8 @@ export default function HostingMaterialDocsOnGHPages() {
                     locale: "pages/HostingMaterialDocsOnGHPagesTutorial",
                     GitHubPagesInitialImage,
                     GitHubPagesSetupImage,
+                    DNSSetup,
+                    GitHubPagesDNSSetup,
                 }}
                 children={content}
             />
